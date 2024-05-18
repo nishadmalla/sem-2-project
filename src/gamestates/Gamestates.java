3258f49c16f98ffd6c72485944e6c5d9ba27f527
nishad <me0again0@gmail.com>
2024-05-18 15:02:37 +0545

    change in game state

--- src/gamestates/Gamestates.java
+++
@@ -1,9 +0,0 @@
-package gamestates;
-
-public enum Gamestates {
-
-    PLAYIING, MENU ;
-
-    public static Gamestates state = MENU;
-    
-}
--- src/gamestates/Menu.java
+++
@@ -1,5 +0,0 @@
-package gamestates;
-
-public class Menu {
-    
-}
