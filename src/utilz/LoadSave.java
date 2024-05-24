865822b9288dabf7fc67ba0c4092744a98800af8
mirajgansi <mirajgansi122@gmail.com>
2024-05-24 09:12:04 +0545

    adding images

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -16,21 +16,21 @@ import static utilz.Constants.EnemyConstants.CRABBY;
 
 public class LoadSave {
 
-	public static final String PLAYER_ATLAS = "player_sprites.png";
-	public static final String LEVEL_ATLAS = "outside_sprites.png";
-	public static final String LEVEL_ONE_DATA = "level_one_data_long.png";
-	public static final String MENU_BUTTONS = "button_atlas.png";
-	public static final String MENU_BACKGROUND = "menu_background.png";
-	public static final String PAUSE_BACKGROUND = "pause_menu.png";
-	public static final String SOUND_BUTTONS = "sound_button.png";
-	public static final String URM_BUTTONS = "urm_buttons.png";
-	public static final String VOLUME_BUTTONS = "volume_buttons.png";
-	public static final String MENU_BACKGROUND_IMG = "background_menu.png";
-	public static final String PLAYING_BG_IMG = "playing_bg_img.png";
-	public static final String BIG_CLOUDS = "big_clouds.png";
-	public static final String SMALL_CLOUDS = "small_clouds.png";
-	public static final String CRABBY_SPRITE = "crabby_sprite.png";
-	public static final String STATUS_BAR = "health_power_bar.png";
+	public static final String PLAYER_ATLAS = "/res/player_sprites.png";
+	public static final String LEVEL_ATLAS = "/res/outside_sprites.png";
+	public static final String MENU_BUTTONS = "/res/button_atlas.png";
+	public static final String MENU_BACKGROUND = "/res/menu_background.png";
+	public static final String PAUSE_BACKGROUND = "/res/pause_menu.png";
+	public static final String SOUND_BUTTONS = "/res/sound_button.png";
+	public static final String URM_BUTTONS = "/res/urm_buttons.png";
+	public static final String VOLUME_BUTTONS = "/res/volume_buttons.png";
+	public static final String MENU_BACKGROUND_IMG = "/res/background_menu.png";
+	public static final String PLAYING_BG_IMG = "/res/playing_bg_img.png";
+	public static final String BIG_CLOUDS = "/res/big_clouds.png";
+	public static final String SMALL_CLOUDS = "/res/small_clouds.png";
+	public static final String CRABBY_SPRITE = "/res/crabby_sprite.png";
+	public static final String STATUS_BAR = "/res/health_power_bar.png";
+	public static final String COMPLETED_IMG = "/res/completed_sprite.png";
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
