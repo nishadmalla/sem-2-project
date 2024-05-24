e8eaef5bee98a7da02f8dde391e525c99e6ea840
mirajgansi <mirajgansi122@gmail.com>
2024-05-24 09:24:18 +0545

    .

--- src/levels/LevelManager.java
+++ src/levels/LevelManager.java
@@ -34,6 +34,21 @@ public class LevelManager {
 		game.getPlaying().getPlayer().loadLvlData(newLevel.getLevelData());
 		game.getPlaying().setMaxLvlOffset(newLevel.getLvlOffset());
 	}
+	private void BuildAllLevels() {
+		BufferedImage[] allLevels=LoadSave.GetAllLevels();
+		for(BufferedImage img : allLevels)
+			levels.add(new Level(img));
+	}
+	private void importOutsideSprites() {
+		BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.LEVEL_ATLAS);
+		levelSprite = new BufferedImage[48];
+		for (int j = 0; j < 4; j++)
+			for (int i = 0; i < 12; i++) {
+				int index = j * 12 + i;
+				levelSprite[index] = img.getSubimage(i * 32, j * 32, 32, 32);
+			}
+	}
+
 
 	public void draw(Graphics g, int lvlOffset) {
 		for (int j = 0; j < Game.TILES_IN_HEIGHT; j++)
