49967bb9443e67ca06217fe8f00637eb7ef74965
mirajgansi <mirajgansi122@gmail.com>
2024-08-07 14:28:29 +0545

    audio added

--- src/Audio/AudioPlayer.java
+++ src/Audio/AudioPlayer.java
@@ -1,6 +1,5 @@
 package Audio;
 
-import java.util.Random;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
@@ -13,9 +12,9 @@ import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-
 public class AudioPlayer {
-    public static int MENU_1 = 0;
+
+	public static int MENU_1 = 0;
 	public static int LEVEL_1 = 1;
 	public static int LEVEL_2 = 2;
 
@@ -60,7 +59,8 @@ public class AudioPlayer {
 			}
 		}
 	}
-    private Clip getClip(String name) {
+
+	private Clip getClip(String name) {
 		System.out.println("Getting clip for " + name);
 		URL url = getClass().getResource("/audioa/" + name + ".wav");
 		if (url == null) {
@@ -92,31 +92,13 @@ public class AudioPlayer {
 			return null;
 		}
 	}
-    public void setVolume(float volume) {
+	public void setVolume(float volume) {
 		this.volume = volume;
 		updateSongVolume();
 		updateEffectsVolume();
 	}
 
-	private void updateEffectsVolume() {
-        for (Clip c : effects) {
-			if (c != null) {
-				FloatControl gainControl = (FloatControl) c.getControl(FloatControl.Type.MASTER_GAIN);
-				float range = gainControl.getMaximum() - gainControl.getMinimum();
-				float gain = (range * volume) + gainControl.getMinimum();
-				gainControl.setValue(gain);
-			}
-        }
-    }
-
-    private void updateSongVolume() {
-        FloatControl gainControl = (FloatControl) songs[currentSongId].getControl(FloatControl.Type.MASTER_GAIN);
-		float range = gainControl.getMaximum() - gainControl.getMinimum();
-		float gain = (range * volume) + gainControl.getMinimum();
-		gainControl.setValue(gain);
-    }
-
-    public void stopSong() {
+	public void stopSong() {
 		if (songs[currentSongId].isActive())
 			songs[currentSongId].stop();
 	}
@@ -127,13 +109,74 @@ public class AudioPlayer {
 		else
 			playSong(LEVEL_2);
 	}
-    public void playSong(int song) {
+
+	public void lvlCompleted() {
+		stopSong();
+		playEffect(LVL_COMPLETED);
+	}
+
+	public void playAttackSound() {
+		int start = 4;
+		start += rand.nextInt(3);
+		playEffect(start);
+	}
+	
+	public void playEffect(int effect) {
+		if (effects[effect] != null) {
+			effects[effect].setMicrosecondPosition(0);
+			effects[effect].start();
+		} else {
+			System.err.println("Error: Clip object is null for effect " + effect);
+		}
+	}
+
+	public void playSong(int song) {
 		stopSong();
 
 		currentSongId = song;
 		updateSongVolume();
 		songs[currentSongId].setMicrosecondPosition(0);
 		songs[currentSongId].loop(Clip.LOOP_CONTINUOUSLY);
+	}
 
-      }
+	public void toggleSongMute() {
+		this.songMute = !songMute;
+		for (Clip c : songs) {
+			BooleanControl booleanControl = (BooleanControl) c.getControl(BooleanControl.Type.MUTE);
+			booleanControl.setValue(songMute);
+		}
+	}
+
+	public void toggleEffectMute() {
+		this.effectMute = !effectMute;
+		for (Clip c : effects) {
+			BooleanControl booleanControl = (BooleanControl) c.getControl(BooleanControl.Type.MUTE);
+			booleanControl.setValue(effectMute);
+		}
+		if (!effectMute)
+			playEffect(JUMP);
+			playEffect(PEARL);
+			playEffect(CRABBY);
+	}
+
+	private void updateSongVolume() {
+
+		FloatControl gainControl = (FloatControl) songs[currentSongId].getControl(FloatControl.Type.MASTER_GAIN);
+		float range = gainControl.getMaximum() - gainControl.getMinimum();
+		float gain = (range * volume) + gainControl.getMinimum();
+		gainControl.setValue(gain);
+
+	}
+
+	private void updateEffectsVolume() {
+		for (Clip c : effects) {
+			if (c != null) {
+				FloatControl gainControl = (FloatControl) c.getControl(FloatControl.Type.MASTER_GAIN);
+				float range = gainControl.getMaximum() - gainControl.getMinimum();
+				float gain = (range * volume) + gainControl.getMinimum();
+				gainControl.setValue(gain);
+			}
+		}
+
+}
 }
