b3b894c805b400c26a1ae9469a9edb50675284c0
nishadmalla <149762693+nishadmalla@users.noreply.github.com>
2024-08-08 19:00:44 +0545

    edit

--- src/gamestates/Gamestate.java
+++ src/gamestates/Gamestate.java
@@ -2,7 +2,7 @@ package gamestates;
 
 public enum Gamestate {
 
-	PLAYING, MENU, OPTIONS, QUIT,LOGIN,SIGNIN;;
+	PLAYING, MENU, OPTIONS, QUIT, CREDITS, PLAYER_SELECTION;
 
 	public static Gamestate state = MENU;
 
